b0e8ab68d27037bc0c5ca2756b260279cb5c0c51
Trzynasty <akam111@wp.pl>
2016-03-06 19:26:16 +0100

    Update Fcfs.java

--- Fcfs.java
+++ Fcfs.java
@@ -1,14 +1,21 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.*;
-import java.io.*;
 public class Fcfs {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner("Dane.txt");
+	public static void main(String[] args) throws FileNotFoundException{
+		int czas=0;
+		FileReader f = new FileReader("Dane.txt");
+		Scanner sc = new Scanner(f);
 		ArrayDeque<Integer> dane = new ArrayDeque<Integer>();
 		 while(sc.hasNextInt()) {
              dane.add(sc.nextInt());
 		 }
 		 sc.close();
+		 while(!dane.isEmpty()){
+			 czas+=dane.remove();
+		 }
+		 System.out.println("Czas = "+Integer.toString(czas)+" ");
 	}
 }
-		
+				
