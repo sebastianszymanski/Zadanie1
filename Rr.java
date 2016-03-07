5d3f89c68b7c47384df7de44ae2ee88bd601933c
Sebastian Szymanski <228060@student.pwr.edu.pl>
2016-03-07 19:57:07 +0100

    Dodanie przseunięć

--- Rr.java
+++ Rr.java
@@ -3,19 +3,32 @@ import java.io.*;
 
 public class Rr{
 	public static void main(String[] args) throws FileNotFoundException {
-
+		int kwantCzasu = 10;
+		int liczus = 0;
+		int czas = 0;
 		Scanner wej = new Scanner(new File("Dane.txt"));
-		Scanner wej2 = new Scanner(new File("Dane.txt"));
-		ArrayDeque<Rekord> danecp = new ArrayDeque<Rekord>();
 		ArrayDeque<Rekord> daneak = new ArrayDeque<Rekord>();
+		ArrayDeque<Rekord> pamiec = new ArrayDeque<Rekord>();
 		int i = 0;
-		while (wej.hasNext())
+		for (int j = 0; j < 10; j++)
 		{
 			i++;
-			danecp.add(new Rekord(i, wej.nextInt(), 0));
+			daneak.add(new Rekord(i, wej.nextInt(), 0));
 		}
+		while (!daneak.isEmpty())
+		{
+			Rekord temp = daneak.remove();
+			if (temp.pozostalyCzasWykonania > kwantCzasu)
+			{
+				temp.pozostalyCzasWykonania -= 10;
+				temp.czasOczekiwania += czas;
+				daneak.add(temp);
+				czas += 10;
+			}
+		}
+		
+		
 		wej.close();
-		wej2.close();
 	}
 
 }
