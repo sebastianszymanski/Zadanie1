1e353c8f24fde9e65a25239b616fc6cb9530e22a
Sebastian Szymanski <228060@student.pwr.edu.pl>
2016-03-07 18:55:18 +0100

    Utworzenie pliku rr

---
+++ Rr.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+
+public class Rr {
+
+	public static void main(String[] args) throws FileNotFoundException {
+
+		Scanner wej = new Scanner(new File("Dane.txt"));
+	}
+
+}
