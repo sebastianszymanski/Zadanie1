b99d185a15fcaa77a5fabfcfd274934909e36eaf
Sebastian Szymanski <228060@student.pwr.edu.pl>
2016-03-07 18:59:07 +0100

    Wczytanie do tablicy

--- Rr.java
+++ Rr.java
@@ -6,6 +6,12 @@ public class Rr {
 	public static void main(String[] args) throws FileNotFoundException {
 
 		Scanner wej = new Scanner(new File("Dane.txt"));
+		ArrayDeque<Integer> dane = new ArrayDeque<Integer>();
+		while (wej.hasNext())
+		{
+			dane.add(wej.nextInt());
+		}
+		wej.close();
 	}
 
 }
