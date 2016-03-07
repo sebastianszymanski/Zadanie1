985dcb339821f01cdba6f7bb50a92f09a9717b5c
Sebastian Szymanski <228060@student.pwr.edu.pl>
2016-03-07 19:22:31 +0100

    Dodanie klasy Rekord

--- Rr.java
+++ Rr.java
@@ -3,15 +3,32 @@ import java.io.*;
 
 public class Rr {
 
+	public class Rekord
+	{
+		public int numerProcesu;
+		public int pozostalyCzasWykonania;
+		public int czasOczekiwania;
+		
+		public Rekord(int np, int pcw, int co)
+		{
+			numerProcesu = np;
+			pozostalyCzasWykonania = pcw;
+			czasOczekiwania = co;
+		}
+	}
+	
 	public static void main(String[] args) throws FileNotFoundException {
 
 		Scanner wej = new Scanner(new File("Dane.txt"));
-		ArrayDeque<Integer> dane = new ArrayDeque<Integer>();
+		Scanner wej2 = new Scanner(new File("Dane.txt"));
+		ArrayDeque<Integer> danecp = new ArrayDeque<Integer>();
+		ArrayDeque<Integer> daneak = new ArrayDeque<Integer>();
 		while (wej.hasNext())
 		{
-			dane.add(wej.nextInt());
+			danecp.add(wej.nextInt());
 		}
 		wej.close();
+		wej2.close();
 	}
 
 }
